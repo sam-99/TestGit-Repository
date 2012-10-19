dfee904835aea982123c394c05ea7232aeaf6668
sam-99 <sam.r1@gmx.com>
2012-10-19 11:33:51 -0400

    checking in dev-sam-99 branch

--- TestGit4/src/com/test/Hello.java
+++ TestGit4/src/com/test/Hello.java
@@ -3,6 +3,6 @@ package com.test;
 public class Hello {
 
 	public Hello() {
-		System.out.println("Hello again1 - Add - not staged");
+		System.out.println("Hello again1 - Add - not staged - dev-sam-99");
 	}
 }
